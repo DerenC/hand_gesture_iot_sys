8e108fb567e73ebccf76020e28eb999d664c6c16
DerenC <chengderen953@gmail.com>
2025-12-29 23:59:04 +0800

    Impl __init__ and _send_to_topic methods for IOTConnection class

--- iot_control.py
+++ iot_control.py
@@ -1,3 +1,13 @@
+import paho.mqtt.client as mqtt
+
 class IOTConnection():
-    def __init__(self):
-        pass
+
+    def __init__(self, topic="home/central", broker="broker.hivemq.com"):
+        self.topic = topic
+        self.broker = broker
+        self.client = mqtt.Client()
+
+        self.client.connect(self.broker, 1883, 60)
+
+    def _send_to_topic(self, message):
+        self.client.publish(self.topic, message)
