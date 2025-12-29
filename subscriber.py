03d5d81ceb2f37e218511c71f74699ce8f44eaaf
DerenC <chengderen953@gmail.com>
2025-12-29 22:56:55 +0800

    Initial impl of publisher & subscriber

---
+++ subscriber.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+
+BROKER = "broker.hivemq.com"
+TOPIC = "iot/example"
+
+def on_message(client, userdata, message):
+    r = int(message.payload.decode())
+    print(f"I receive: {r}")
+
+client = mqtt.Client()
+client.on_message = on_message
+client.connect(BROKER, 1883, 60)
+client.subscribe(TOPIC)
+
+print("Listening to the messages...")
+
+client.loop_forever()
