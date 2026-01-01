e93cfde89d9a2d7cd81012f01ddea75ffad85be2
DerenC <chengderen953@gmail.com>
2026-01-02 01:24:40 +0800

    Add rounding-off for dist_between() function

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import math
 
+NUM_OF_DP = 6
+
 def dist_between(x1, y1, x2, y2):
-    return math.sqrt( (x1 - x2)**2 + (y1 - y2)**2)
+    return round(math.sqrt( (x1 - x2)**2 + (y1 - y2)**2), NUM_OF_DP)
