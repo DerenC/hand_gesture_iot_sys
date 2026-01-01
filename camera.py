74099929ea8996eb942964a53b482622dd48f2a2
DerenC <chengderen953@gmail.com>
2026-01-01 23:31:52 +0800

    Add code in camera.py to get & test finger measurement data

--- camera.py
+++ camera.py
@@ -4,12 +4,16 @@ from enum import Enum
 
 import mediapipe as mp
 
+import math
+
 class Finger(Enum):
     THUMB = 4
     INDEX = 8
     MIDDLE = 12
     RING = 16
     PINKY = 20
+
+finger_id_map = {s: enum.value for s, enum in Finger._member_map_.items()}
 
 ALL_FINGERS = [Finger.THUMB, Finger.INDEX, Finger.MIDDLE, Finger.RING, Finger.PINKY]
 
@@ -44,6 +48,10 @@ class HandGestureTracker(IOTConnection):
         self.mp_draw = mp.solutions.drawing_utils
         self.command = None
         self.prev_command = None
+        self.curr_lm_state = ""
+        self.prev_lm_state = ""
+        self.lm0_x = None
+        self.lm0_y = None
 
     def _hand_finder(self, image, draw=True):
         img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -62,10 +70,52 @@ class HandGestureTracker(IOTConnection):
             for id, lm in enumerate(hand.landmark):
                 h, w, _ = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                self.lm_list.append([id, cx, cy])
+                self.lm_list.append([id, cx, cy, lm.x, lm.y])
 
                 if id == 0 and draw:
+                    self.lm0_x = lm.x
+                    self.lm0_y = lm.y
                     cv2.circle(image, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+    def _get_finger_dist(self, id):
+        '''
+        4 -- Thumb
+        8 -- index
+        12 -- Middle
+        16 -- Ring
+        20 -- Pinky
+        '''
+        if self.lm0_x is None or self.lm0_y is None: return None
+        for lm in self.lm_list:
+            if lm[0] == id: return math.sqrt((lm[3] - self.lm0_x)**2 +(lm[4] - self.lm0_y)**2)
+        return None
+
+    def _loop_print_state(self):
+        self.curr_lm_state = self._print_and_return_state()
+        if self.curr_lm_state != self.prev_lm_state:
+            self.prev_lm_state = self.curr_lm_state
+            print(self.curr_lm_state)
+
+    def _print_and_return_state(self):
+        if len(self.curr_lm_state) == 0: return ""
+        state = ""
+        for finger, id in finger_id_map.items():
+            finger_dist = self._get_finger_dist(id)
+            if finger_dist is None: continue
+            finger_lm_description = f"{finger}: {finger_dist}\n"
+            state += finger_lm_description
+        return state
+
+    def _print_state(self):
+        printed = False
+
+        for finger, id in finger_id_map.items():
+            finger_dist = self._get_finger_dist(id)
+            if finger_dist is None: continue
+            if not printed: printed = True
+            print(f"{finger}: {finger_dist}")
+            
+        if printed: print()
 
     def _finger_down(self, fingers=[]):
         heights = []
@@ -124,6 +174,7 @@ class HandGestureTracker(IOTConnection):
         if self.command != self.prev_command:
             self.prev_command = self.command
             # send the message to broker
+            self._send_to_topic(self.command.name)
 
     def run(self, show=True):
         cap = cv2.VideoCapture(0)
@@ -144,6 +195,28 @@ class HandGestureTracker(IOTConnection):
         cap.release()
         cv2.destroyAllWindows()
 
+    def test(self, show=True):
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            success, frame = cap.read()
+
+            if not success: break
+
+            frame = self._hand_finder(frame)
+            self._position_finder(frame)
+            # self._loop_print_state()
+            self._print_state()
+
+            if show: cv2.imshow("Hand Tracking", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"): break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
 if __name__ == "__main__":
     tracker = HandGestureTracker()
-    tracker.run()
+    # tracker.run()
+    tracker.test()
+
