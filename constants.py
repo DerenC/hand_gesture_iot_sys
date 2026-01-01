b0058e88cc96f016d05a9b90fa8c4f63f39a016f
DerenC <chengderen953@gmail.com>
2026-01-02 00:25:35 +0800

    Adding REF_BASED_RATIOS and SCALE_DOWN_FACTOR in constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+from enums import FingerLM
+
+REF_BASED_RATIOS = {
+    FingerLM.THUMB: 3.6,
+    FingerLM.INDEX: 6.6,
+    FingerLM.MIDDLE: 7.2,
+    FingerLM.RING: 7.0,
+    FingerLM.PINKY: 5.9,
+}
+
+SCALE_DOWN_FACTOR = 0.75
